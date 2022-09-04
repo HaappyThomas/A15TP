e92e5294d7125452fb27dcccf69ae27e99eaae14
HaappyThomas <happy.thomas.wang@gmail.com>
2022-09-03 20:07:56 -0400

    finir models class Tache et Utilisateur

--- src/main/java/ca/qc/bdeb/models/Tache.java
+++ src/main/java/ca/qc/bdeb/models/Tache.java
@@ -1,5 +1,72 @@
 package ca.qc.bdeb.models;
 
-public class Tache {
+import java.time.*;
 
+public class Tache {
+	private int tacheId;
+	private String description;
+	private LocalDate datetime;
+	private int duree;
+	private int utilisateurId;
+	
+	public Tache() {
+	}
+
+	public Tache(int tacheId, String description, LocalDate datetime, int duree, int utilisateurId) {
+		this.tacheId = tacheId;
+		this.description = description;
+		this.datetime = datetime;
+		this.duree = duree;
+		this.utilisateurId = utilisateurId;
+	}
+
+	public int getTacheId() {
+		return tacheId;
+	}
+
+	public void setTacheId(int tacheId) {
+		this.tacheId = tacheId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDate getDatetime() {
+		return datetime;
+	}
+
+	public void setDatetime(LocalDate datetime) {
+		this.datetime = datetime;
+	}
+
+	public int getDuree() {
+		return duree;
+	}
+
+	public void setDuree(int duree) {
+		this.duree = duree;
+	}
+
+	public int getUtilisateurId() {
+		return utilisateurId;
+	}
+
+	public void setUtilisateurId(int utilisateurId) {
+		this.utilisateurId = utilisateurId;
+	}
+
+	@Override
+	public String toString() {
+		return "Tache [tacheId=" + tacheId + ", description=" + description + ", datetime=" + datetime + ", duree="
+				+ duree + ", utilisateurId=" + utilisateurId + "]";
+	}
+	
+	
+	
+	
 }
--- src/main/java/ca/qc/bdeb/models/Utilisateur.java
+++ src/main/java/ca/qc/bdeb/models/Utilisateur.java
@@ -1,5 +1,42 @@
 package ca.qc.bdeb.models;
 
 public class Utilisateur {
+	private int utilisateurId;
+	private String nomComplet;
+	
+	public Utilisateur() {
+		super();
+	}
+
+	public Utilisateur(int utilisateurId, String nomComplet) {
+		super();
+		this.utilisateurId = utilisateurId;
+		this.nomComplet = nomComplet;
+	}
+
+	public int getUtilisateurId() {
+		return utilisateurId;
+	}
+
+	public void setUtilisateurId(int utilisateurId) {
+		this.utilisateurId = utilisateurId;
+	}
+
+	public String getNomComplet() {
+		return nomComplet;
+	}
+
+	public void setNomComplet(String nomComplet) {
+		this.nomComplet = nomComplet;
+	}
+
+	@Override
+	public String toString() {
+		return "Utilisateur [utilisateurId=" + utilisateurId + ", nomComplet=" + nomComplet + "]";
+	}
+	
+	
+	
+	
 
 }
