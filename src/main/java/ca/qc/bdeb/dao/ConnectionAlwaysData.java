f1fbf0024a66b7c72b4426976e543f52b5fb0708
HaappyThomas <happy.thomas.wang@gmail.com>
2022-09-03 22:14:05 -0400

    modification de test classe pour test table TbL_Tache

--- src/main/java/ca/qc/bdeb/dao/ConnectionAlwaysData.java
+++ src/main/java/ca/qc/bdeb/dao/ConnectionAlwaysData.java
@@ -31,6 +31,4 @@ public class ConnectionAlwaysData {
 		return connect;
 	}
 	
-	
-	
 }
--- test/ca/qc/bdeb/dao/ConnectionAlwaysDataTest.java
+++ test/ca/qc/bdeb/dao/ConnectionAlwaysDataTest.java
@@ -10,16 +10,16 @@ import ca.qc.bdeb.models.Tache;
 import ca.qc.bdeb.models.Utilisateur;
 
 public class ConnectionAlwaysDataTest {
-	
+
 	@Test
 	public void connectTableUtilisateurTest() {
 		Connection connection = ConnectionAlwaysData.getInstance();
-		
+
 		try {
 			Statement stmt = connection.createStatement();
 			ResultSet rs = stmt.executeQuery("select * from TbL_Utilisateur");
 //			List<Utilisateur> utilisateurs = new ArrayList<>();
-			while(rs.next()) {
+			while (rs.next()) {
 				Utilisateur utilisateur = new Utilisateur();
 				utilisateur.setUtilisateurId(rs.getInt(1));
 				utilisateur.setNomComplet(rs.getString(2));
@@ -29,7 +29,31 @@ public class ConnectionAlwaysDataTest {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
-		
+	}
+
+	@Test
+	public void connectTableTacheTest() {
+		Connection connection = ConnectionAlwaysData.getInstance();
+
+		try {
+			Statement stmt = connection.createStatement();
+			ResultSet rs;
+			rs = stmt.executeQuery("select * from TbL_Tache");
+//			List<Tache> taches = new ArrayList<>();
+
+			while (rs.next()) {
+				Tache tache = new Tache();
+				tache.setTacheId(rs.getInt(1));
+				tache.setDescription(rs.getString(2));
+				tache.setDatetime(rs.getDate(3).toLocalDate());
+				tache.setUtilisateurId(rs.getInt(4));
+				
+//				taches.add(tache);
+				System.out.println(tache);
+				
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
